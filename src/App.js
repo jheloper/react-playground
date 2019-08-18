0ab4dec1c8e2b2ced2796e1115122f17488d7d5b
Joonghyeon Kim <jheloper9102@gmail.com>
2019-08-18 22:43:17 +0900

    JSX에서 div 요소 대신 Fragment를 사용하도록 수정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <Fragment>
       <h1>Hello React!</h1>
       <h2>당신은 어썸한가요?</h2>
-    </div>
+    </Fragment>
   );
 }
 
