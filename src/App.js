5b279be76175a69e2fb40730a805f30d2ee472d8
Joonghyeon Kim <jheloper9102@gmail.com>
2019-08-20 02:35:54 +0900

    JSX && 표현식 사용 및 인라인 스타일링 예제 코드 추가

--- src/App.js
+++ src/App.js
@@ -3,14 +3,25 @@ import './App.css';
 
 class App extends Component {
   render() {
-    const text = '당신은 어썸한가요?';
+    const text = 'Are you awesome?';
     const condition = true;
+    const style = {
+      backgroundColor: 'gray',
+      border: '1px solid black',
+      height: Math.round(Math.random() * 300) + 50,
+      width: Math.round(Math.random() * 300) + 50,
+      WebkitTransition: 'all',
+      MozTransition: 'all',
+      msTransition: 'all'
+    }
 
     return (
       <Fragment>
         <h1>Hello React!</h1>
         <h2>{text}</h2>
-        {condition ? '참' : '거짓'}
+        {condition ? 'True' : 'False'}
+        {condition && 'Show me'}
+        <div style={style}></div>
       </Fragment>
     );
   }
