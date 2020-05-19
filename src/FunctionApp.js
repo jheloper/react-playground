276a79e4a5fbb4b7d9ed56a39b6de8594e1a329f
Joonghyeon Kim <jheloper9102@gmail.com>
2020-05-19 23:57:06 +0900

    JSX 문법에 대한 예제 코드 추가

--- src/FunctionApp.js
+++ src/FunctionApp.js
@@ -2,12 +2,20 @@ import React, { Fragment } from "react";
 import "./App.css";
 
 function FunctionApp() {
-    const name = "리액트";
+    const name = "리액트";
+    const name2 = undefined;
     return (
         <Fragment>
             Hello, <b>react</b>
             <h1>{name} 안녕!</h1>
             <h2>잘 작동하니?</h2>
+            {name === "리액트" ? (
+                <h1>리액트입니다.</h1>
+            ) : (
+                <h2>리액트가 아닙니다.</h2>
+            )}
+            {name === "리액트" && <h1>리액트입니다. AND 연산자 사용했을때!</h1>}
+            {name2 || "값이 undefined입니다."}
         </Fragment>
     );
 }
