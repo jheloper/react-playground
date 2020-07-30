774d46b42ef115b05b423ea72afa5ded8a5acff9
Joonghyeon Kim <jheloper9102@gmail.com>
2020-07-30 18:06:22 +0900

    default props, children props, 비구조화 할당으로 props 수정

--- src/FunctionApp.js
+++ src/FunctionApp.js
@@ -2,7 +2,7 @@ import React from "react";
 import MyComponent from "./MyFunctionComponent";
 
 const App = () => {
-    return <MyComponent name="React" />;
+    return <MyComponent>React</MyComponent>;
 };
 
 export default App;
--- src/MyFunctionComponent.js
+++ src/MyFunctionComponent.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const MyComponent = (props) => {
-    return <div>Hi, my name is {props.name}.</div>;
+    const { name, children } = props;
+    return (
+        <div>
+            Hi, my name is {name}.<br />
+            children props is {children}.
+        </div>
+    );
+};
+
+MyComponent.defaultProps = {
+    name: "Default name",
 };
 
 export default MyComponent;
