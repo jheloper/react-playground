44fbeecbf60afb18340e169f11d946167c144ab6
Joonghyeon Kim <jheloper9102@gmail.com>
2020-07-30 18:01:16 +0900

    함수형 컴포넌트로 예제 컴포넌트 변경

--- src/FunctionApp.js
+++ src/FunctionApp.js
@@ -1,23 +1,8 @@
-import React, { Fragment } from "react";
-import "./App.css";
+import React from "react";
+import MyComponent from "./MyFunctionComponent";
 
-function FunctionApp() {
-    const name = "리액트";
-    const name2 = undefined;
-    return (
-        <Fragment>
-            Hello, <b>react</b>
-            <h1>{name} 안녕!</h1>
-            <h2>잘 작동하니?</h2>
-            {name === "리액트" ? (
-                <h1>리액트입니다.</h1>
-            ) : (
-                <h2>리액트가 아닙니다.</h2>
-            )}
-            {name === "리액트" && <h1>리액트입니다. AND 연산자 사용했을때!</h1>}
-            {name2 || "값이 undefined입니다."}
-        </Fragment>
-    );
-}
+const App = () => {
+    return <MyComponent name="React" />;
+};
 
-export default FunctionApp;
+export default App;
---
+++ src/MyFunctionComponent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyComponent = (props) => {
+    return <div>Hi, my name is {props.name}.</div>;
+};
+
+export default MyComponent;
