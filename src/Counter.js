5a57f13bc2ee9d6cf141605d83889cc869d901a5
Joonghyeon Kim <jheloper9102@gmail.com>
2020-07-30 18:46:33 +0900

    setState 완료 후 호출할 콜백 함수를 2번째 인자로 전달하도록 수정

--- src/Counter.js
+++ src/Counter.js
@@ -27,18 +27,29 @@ class Counter extends Component {
                         // this.setState({ number: number + 1 });
 
                         // setState에 객체 대신 함수를 인자로 전달
-                        this.setState((prevState, props) => {
-                            return {
-                                number: prevState.number + 1,
-                            };
-                        });
+                        // this.setState((prevState, props) => {
+                        //     return {
+                        //         number: prevState.number + 1,
+                        //     };
+                        // });
                         // 위 코드와 아래 코드는 같은 기능이지만, 아래 코드는 함수에서 바로 객체를 반환한다는 의미.
-                        this.setState((prevState, props) => ({
-                            number: prevState.number + 1
-                        }));
+                        // this.setState((prevState, props) => ({
+                        //     number: prevState.number + 1
+                        // }));
+
+                        // setState 끝난 후 실행할 콜백 함수를 2번째 인자로 전달
+                        this.setState(
+                            {
+                                number: number + 1,
+                            },
+                            () => {
+                                console.log("Call setState.");
+                                console.log(this.state);
+                            }
+                        );
                     }}
                 >
-                    +2
+                    +1
                 </button>
             </div>
         );
