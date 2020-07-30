bc9efe2192db2ade25d69b1877f4a51367741598
Joonghyeon Kim <jheloper9102@gmail.com>
2020-07-30 18:09:52 +0900

    함수 컴포넌트 파라미터 부분에 비구조화 할당 적용 및 propTypes 적용

--- src/MyFunctionComponent.js
+++ src/MyFunctionComponent.js
@@ -1,7 +1,8 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const MyComponent = (props) => {
-    const { name, children } = props;
+// 비구조화 할당 문법을 함수 파라미터 부분에 적용
+const MyComponent = ({ name, children }) => {
     return (
         <div>
             Hi, my name is {name}.<br />
@@ -10,8 +11,14 @@ const MyComponent = (props) => {
     );
 };
 
+// props 기본값 설정
 MyComponent.defaultProps = {
     name: "Default name",
+};
+
+// propTypes로 props 검증
+MyComponent.propTypes = {
+    name: PropTypes.string
 };
 
 export default MyComponent;
