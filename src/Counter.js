c061ebb72498a747a231843f0e05bf89aeeed4b2
Joonghyeon Kim <jheloper9102@gmail.com>
2020-07-30 18:30:39 +0900

    클래스형 컴포넌트 생성자 없이 state 초기값 설정하도록 수정

--- src/Counter.js
+++ src/Counter.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-    constructor(props) {
-        super(props);
-        // state 초기값 설정
-        this.state = {
-            number: 0,
-            fixedNumber: 0,
-        };
-    }
+    // constructor(props) {
+    //     super(props);
+    //     // state 초기값 설정
+    //     this.state = {
+    //         number: 0,
+    //         fixedNumber: 0,
+    //     };
+    // }
+
+    state = {
+        number: 0,
+        fixedNumber: 0,
+    };
 
     render() {
         const { number, fixedNumber } = this.state;
