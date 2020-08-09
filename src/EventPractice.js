22bb5e27907494ed1a41281f8ca8cb1194e959ef
Joonghyeon Kim <jheloper9102@gmail.com>
2020-08-09 16:33:40 +0900

    여러 input에 대응하기 위해 onChange 함수 수정 및 keyPress 함수 추가

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -2,18 +2,25 @@ import React, { Component } from "react";
 
 class EventPractice extends Component {
     state = {
+        username: "",
         message: "",
     };
 
     handleChange = (e) => {
         console.log(e);
-        this.setState({ message: e.target.value });
+        this.setState({ [e.target.name]: e.target.value });
     };
 
     handleClick = () => {
-        alert(this.state.message);
-        this.setState({ message: "" });
-    }
+        alert(this.state.username + ": " + this.state.message);
+        this.setState({ username: "", message: "" });
+    };
+
+    handleKeyPress = (e) => {
+        if (e.key === "Enter") {
+            this.handleClick();
+        }
+    };
 
     render() {
         return (
@@ -21,10 +28,18 @@ class EventPractice extends Component {
                 <h1>Event Practice</h1>
                 <input
                     type="text"
+                    name="username"
+                    placeholder="User name"
+                    value={this.state.username}
+                    onChange={this.handleChange}
+                ></input>
+                <input
+                    type="text"
                     name="message"
                     placeholder="Input anything"
                     value={this.state.message}
                     onChange={this.handleChange}
+                    onKeyPress={this.handleKeyPress}
                 ></input>
                 <button onClick={this.handleClick}>확인</button>
             </div>
